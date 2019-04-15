f8a17169956aa5f5cb1df8bf452df41822171958
TheBolshe <bibsim91@gmail.com>
2019-04-16 01:46:29 +0200

    ignore

--- code/_build/test.ml
+++ code/_build/test.ml
@@ -16,23 +16,26 @@ let create_renderer w =
   | Error (`Msg e) -> Sdl.log "Failed create renderer : %s" e; Sdl.destroy_window w; Sdl.quit (); exit 1
   | Ok r -> r
 
+let display renderer =
+  match load_image renderer "../data/pictures/fish.jpg" with
+  | Error (`Msg e) -> Sdl.log "Failed create texture : %s" e
+  | Ok tex -> Sdl.render_clear renderer;
+    render_texture renderer (0,480,0,640) tex Sdl.Flip.none;
+    Sdl.render_present renderer;
+    match load_image renderer "../data/pictures/fish.bmp" with
+    | Error (`Msg e) -> Sdl.log "Failed create texture : %s" e
+    | Ok tex -> render_texture renderer (0,240,0,320) tex Sdl.Flip.none;
+      Sdl.render_present renderer
 
 let () =
   let window = open_window in
   let renderer = create_renderer window in
-  match load_image renderer "../data/pictures/fish.jpg" with
-  | Error (`Msg e) -> Sdl.log "Failed create texture : %s" e
-  | Ok tex -> tex;
-    Sdl.render_clear renderer;
-    render_texture renderer (0,480,0,640) tex Sdl.Flip.none;
-    Sdl.render_present renderer;
-    Sdl.delay 1000l;
-    match load_image renderer "../data/pictures/fish.bmp" with
-    | Error (`Msg e) -> Sdl.log "Failed create texture : %s" e
-    | Ok tex -> tex;
-      render_texture renderer (0,240,0,320) tex Sdl.Flip.none;
-      Sdl.render_present renderer;
-      Sdl.delay 1000l;
-      Sdl.destroy_window window;
-      Sdl.quit ();
-      exit 1
+  let event = Sdl.Event.create () in
+  Sdl.delay 1000l;
+  Sdl.destroy_window window;
+  Sdl.quit ();
+  exit 1
+
+
+let rec loop renderer event =
+  if Sdl.poll_event event 
